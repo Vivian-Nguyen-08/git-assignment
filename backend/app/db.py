d791968ab9c1376a1025f1e74320137f3f00f30e
Eric Lewellen <thesenatesidious@gmail.com>
2025-03-29 12:27:22 -0500

    updated env to be compatible with docker setup

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,22 +1,23 @@
-from sqlmodel import SQLModel, create_engine, Session 
+from sqlmodel import SQLModel, create_engine, Session
 from dotenv import load_dotenv
 import os
 
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL") 
+DATABASE_USER = os.getenv("POSTGRES_USER")
+DATABASE_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+DATABASE_HOST = os.getenv("POSTGRES_HOST")
+DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:5432/planora"
 
-print(DATABASE_URL)
-#create engine 
 engine = create_engine(DATABASE_URL, echo=True)
 with engine.connect() as connection:
     print("Connected to PostgreSQL!")
-    
-def init_db(): 
-    #looks at the models in models.py and all of the models with true will represent database tables 
-    #will then create the tables in the databases 
+
+def init_db():
+    #looks at the models in models.py and all of the models with true will represent database tables
+    #will then create the tables in the databases
     SQLModel.metadata.create_all(engine)
-    
-def get_session(): 
-    with Session(engine) as session: 
-        yield session 
+
+def get_session():
+    with Session(engine) as session:
+        yield session
