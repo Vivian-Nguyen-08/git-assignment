78fc9626db925fdf15367228531dcc9abd892622
Eric Lewellen <thesenatesidious@gmail.com>
2025-04-19 20:05:28 -0500

    changed route for groups to just /groups

--- backend/app/models.py
+++ backend/app/models.py
@@ -29,7 +29,6 @@ class Group(SQLModel, table=True):
         link_model=UserGroupLink
     )
 
-
 class User(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     username: str = Field(unique=True, nullable=False)
