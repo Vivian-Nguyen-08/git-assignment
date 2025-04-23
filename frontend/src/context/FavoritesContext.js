123570271697779d6bb7935819565ba1a0594cda
RohitPenna <pennaro06@gmail.com>
2025-04-23 17:52:39 -0500

    FavoritesContext fixed

--- frontend/src/context/FavoritesContext.js
+++ frontend/src/context/FavoritesContext.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { createContext, useContext, useState, useEffect, useCallback } from "react";
 import { getFavoriteGroups, toggleFavoriteStatus } from "../api";
 import React from "react";
